b8a875d0113fc566530ae27d4ef5e159acf4fb8f
Matthew Pancia <mpancia@gmail.com>
2014-06-01 14:28:04 -0500

    fixed

---
+++ README.md
@@ -0,0 +1,5 @@
+###README
+
+This is a small Flask app that takes in a Twitter handle and displays visualizations of relevant statistics. 
+
+This requires Twitter API keys, which should be set as environment variables as referenced in "run.py". 
--- run.py
+++ run.py
@@ -3,32 +3,33 @@ from flask import render_template, session, url_for, request, redirect
 import pandas as pd 
 import collections
 import os
+import string 
 from os import environ 
 import tweepy
 import json 
 app = Flask(__name__)
 app.config.update(
-    DEBUG = True,
+	DEBUG = True,
 )
 def letter_freq(tweetStream, handle = "matty_books"):
-	data = tweetStream.user_timeline(handle, count=100)
+	data = tweetStream.user_timeline(screen_name = handle, count = 100)
 	tweetString = str()
 	for tweet in data:
 		tweetString = tweetString + tweet.text.lower() + " "
 		counter = collections.Counter(tweetString)
 		n = sum(counter.values())
-		return {char.encode('ascii', 'ignore') : float(count) / n for char, count in counter.most_common() if char != ' ' and char != '@'}
+	return {char.encode('ascii', 'ignore') : float(count) / n for char, count in counter.most_common() if char in string.lowercase}
 
 def freq_to_df(freq):
-    return pd.DataFrame(freq.items(), columns = ['letter', 'frequency'])
+	return pd.DataFrame(freq.items(), columns = ['letter', 'frequency'])
 
-app.secret_key = 'why would I tell you my secret key?'
+app.secret_key = environ.get('global_secret')
 
 @app.route('/',methods=('GET', 'POST'))
 @app.route('/index',methods=('GET', 'POST'))
 def index():
 	if request.method == 'POST':
-		session['handle'] = request.form.get("handle", None)
+		session['handle'] = request.form.get("handle")
 	return render_template("index.html")
 
 @app.route('/handle')
@@ -37,21 +38,21 @@ def login():
 
 @app.route('/letter')
 def letter():
-    consumer_key = environ.get('c_key')
-    consumer_secret = environ.get('c_sec')
-    access_token_key = environ.get('a_key')
-    access_token_secret = environ.get('a_sec')
-    auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
-    auth.set_access_token(access_token_key, access_token_secret)
-    api = tweepy.API(auth, secure=True)
-    freq = freq_to_df(letter_freq(api,session['handle']))
-    freq = freq.ix[freq.letter != ' ']
-    freq = freq.ix[freq.letter != '' ]
-    freq = freq.ix[freq.letter != '"' ]
-    freq = freq.sort('letter')
-    freq = freq.to_json(orient='records')
-    freq = freq[1:-1]
-    return render_template("letter.html", freq=freq)
+	consumer_key = environ.get('c_key')
+	consumer_secret = environ.get('c_sec')
+	access_token_key = environ.get('a_key')
+	access_token_secret = environ.get('a_sec')
+	auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+	auth.set_access_token(access_token_key, access_token_secret)
+	api = tweepy.API(auth, secure=True)
+	freq = freq_to_df(letter_freq(api,session['handle']))
+	freq = freq.ix[freq.letter != ' ']
+	freq = freq.ix[freq.letter != '' ]
+	freq = freq.ix[freq.letter != '"' ]
+	freq = freq.sort('letter')
+	freq = freq.to_json(orient='records')
+	freq = freq[1:-1]
+	return render_template("letter.html", freq=freq)
 
 @app.route('/word')
 def word():
@@ -67,5 +68,5 @@ def set_client_session():
 		session['handle'] = None
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port, debug = True)
